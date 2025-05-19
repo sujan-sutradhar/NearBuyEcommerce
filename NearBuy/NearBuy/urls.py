dc4db11b1c00c7b1dc1850c3f15285aa3423460c
ciphersujan <146098451+Sujan4002@users.noreply.github.com>
2025-05-20 00:09:29 +0530

    static file setup completed

--- NearBuy/NearBuy/urls.py
+++ NearBuy/NearBuy/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+import os 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',include('Store.urls'))
+] + static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
